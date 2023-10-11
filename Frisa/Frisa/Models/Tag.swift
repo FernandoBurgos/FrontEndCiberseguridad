7aaed60a5ea64a6603d9bf9b5e4333b6eccfa4f3
raxhacks <alexhacker654@gmail.com>
2023-10-10 19:42:25 -0600

    tag

--- Frisa/Frisa/Models/Tag.swift
+++ Frisa/Frisa/Models/Tag.swift
@@ -6,8 +6,49 @@
 //
 
 import Foundation
+import Alamofire
 
-struct Tag: Identifiable {
-    var id = UUID()
+struct Tag {
     var name: String
+}
+
+func createTag(tag: Tag) async throws -> Bool {
+    let url = apiURL + "/api/v1/createTag"
+    let session = Session(interceptor:  AccessTokenAdapter());
+    return try await withCheckedThrowingContinuation { continuation in
+        let tagDict: [String: Any] = [
+            "name":tag.name
+        ]
+        session.request(url, method: .post, parameters: tagDict, encoding: JSONEncoding.default)
+                .responseJSON { response in
+                    switch response.result {
+                    case .success(let data):
+                        print(data)
+                        // Process the response data here if needed.
+                        continuation.resume(returning: true) // Return true on success.
+                    case .failure(let error):
+                        print(error)
+                        continuation.resume(throwing: error) // Throw an error on failure.
+                    }
+                }
+        }
+}
+
+func getTag(id: String) async throws -> Bool {
+    let url = apiURL + "/api/v1/getTag/\(id)"
+    let session = Session(interceptor:  AccessTokenAdapter());
+    return try await withCheckedThrowingContinuation { continuation in
+            session.request(url, method: .get)
+                .responseJSON { response in
+                    switch response.result {
+                    case .success(let data):
+                        print(data)
+                        // Process the response data here if needed.
+                        continuation.resume(returning: true) // Return true on success.
+                    case .failure(let error):
+                        print(error)
+                        continuation.resume(throwing: error) // Throw an error on failure.
+                    }
+                }
+        }
 }
